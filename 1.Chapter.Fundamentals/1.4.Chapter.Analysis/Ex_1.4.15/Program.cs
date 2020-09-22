d723a330d4ca911ebb54c91efa96f325fb7bd0b6
xeekseven <xeekseven@outlook.com>
2020-09-22 23:51:32 +0800

    习题提交

---
+++ 1.Chapter.Fundamentals/1.4.Chapter.Analysis/Ex_1.4.15/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex_1._4._15
+{
+    class Program
+    {
+        /// <summary>
+        /// O(N) 2-sum
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int TwoSumFaster(int[] nums)
+        {
+            int count = 0;
+            int i = 0, h = nums.Length - 1;
+            while (i < h)
+            {
+                int t = nums[i] + nums[h];
+                if (t == 0)
+                {
+                    count++;
+                    i++;
+                    h--;
+                }
+                else if (t > 0)
+                {
+                    h--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// O(N^2) 3-sum
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int ThreeSumFaster(int[] nums)
+        {
+            int count = 0;
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                int i = index + 1, h = nums.Length - 1;
+                while (i < h)
+                {
+                    int t = nums[i] + nums[index] + nums[h];
+                    if (t == 0)
+                    {
+                        count++;
+                        i++;
+                        h--;
+                    }
+                    else if (t > 0)
+                    {
+                        h--;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+
+            var array = Enumerable.Range(-443,2327).ToArray();
+
+            Console.WriteLine(ThreeSumFaster(array));
+        }
+    }
+}
---
+++ 1.Chapter.Fundamentals/1.4.Chapter.Analysis/Ex_1.4.16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex_1._4._16
+{
+    class Program
+    {
+        /// <summary>
+        /// O(NLogN) 最近的两个数
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static double[] ClosestTwo(double[] array)
+        {
+            var list = array.ToList();
+            list.Sort((a, b) => a.CompareTo(b));
+            double[] rs = new double[2] { -1, -1 };
+            double min = double.MaxValue;
+            for (int index = 0; index < list.Count - 1; index++)
+            {
+                double r = Math.Abs(list[index + 1] - list[index]);
+                if (min > r)
+                {
+                    min = r;
+                    rs[0] = list[index];
+                    rs[1] = list[index + 1];
+                }
+            }
+            return rs;
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new List<double>();
+            for (int i = 0; i < 10000; i++)
+            {
+                double r1 = (new Random()).NextDouble() + (new Random()).Next(-2434233, 2434233);
+                list.Add(r1);
+            }
+            var rs1 = ClosestTwo(list.ToArray());
+            Console.WriteLine($"ClosestTwo rs:{rs1[0]},{rs1[1]}");
+        }
+    }
+}
