1f65b9f6566d96eb41f45018a1bb84fdba352d4e
xeekseven <xeekseven@outlook.com>
2020-10-22 00:28:18 +0800

    基于线性探针的可扩容散列表

--- 3.Chapter.Lookup/SymbolTable/Program.cs
+++ 3.Chapter.Lookup/SymbolTable/Program.cs
@@ -8,7 +8,8 @@ namespace SymbolTable
         {
             //ArrayST<string,int> st = new ArrayST<string, int>(100);
             //BinarySearchST<string,int> st = new BinarySearchST<string, int>(100);
-            ZipperHashTableST<string,int> st = new ZipperHashTableST<string, int>(8);
+            //ZipperHashTableST<string,int> st = new ZipperHashTableST<string, int>(8);
+            LinearProbingHashST<String,int> st = new LinearProbingHashST<string, int>(2);
             st.Put("A",1);
             Console.WriteLine(st.Get("A")?.Value);
             st.Put("B",2);
