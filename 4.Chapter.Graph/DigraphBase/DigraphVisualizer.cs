9229701ac4348d2a0f9bb901bd43c0220eedd595
xeekseven <xeekseven@outlook.com>
2020-11-01 20:18:21 +0800

    有向图的基础提交

---
+++ 4.Chapter.Graph/DigraphBase/DigraphVisualizer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace DigraphBase
+{
+    public class DigraphVisualizer
+    {
+        public Kind kind = new Kind();
+        public List<Node> nodes = new List<Node>();
+        public List<Edge> edges = new List<Edge>();
+
+        public class Kind
+        {
+            public bool graph = true;
+        }
+        public class Node
+        {
+            public string id;
+            public string label;
+        }
+
+        public class Edge
+        {
+            public string from;
+            public string to;
+            public string label = string.Empty;            
+        }
+    }
+}
