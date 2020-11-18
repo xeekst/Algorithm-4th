d3e657d2265f93ec4543426862a683f5434df92d
xeekst <xeekst@outlook.com>
2020-11-18 23:30:06 +0800

    高位优先排序算法

---
+++ 5.Chapter.Strings/StringsBase/InsertSort.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace StringsBase
+{
+    public class InsertSort
+    {
+        public static void Sort(string[] a, int lo, int hi, int d)
+        {
+            for (int i = lo; i < hi + 1; i++)
+            {
+                for (int j = i; j > lo && Less(a[j], a[j - 1], d); j--)
+                {
+                    Swap(a, j, j - 1);
+                }
+            }
+        }
+
+        private static bool Less(string a, string b, int d)
+        {
+            if (a.Substring(d).CompareTo(b.Substring(d)) < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void Swap(string[] a, int i, int j)
+        {
+            string tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    }
+}
---
+++ 5.Chapter.Strings/StringsBase/MSD.cs
@@ -0,0 +1,62 @@
+namespace StringsBase
+{
+    public class MSD
+    {
+        private static int R = 256;
+        private static int M = 15;
+        private static string[] _aux;
+        private static int CharAt(string s, int d)
+        {
+            if (d < s.Length)
+            {
+                return s[d];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static void Sort(string[] a)
+        {
+            int N = a.Length;
+            _aux = new string[N];
+        }
+
+        private static void Sort(string[] a, int lo, int hi, int d)
+        {
+            if (hi <= lo + M)
+            {
+                InsertSort.Sort(a, lo, hi, d); return;
+            }
+
+            int[] count = new int[R + 2];
+            // 频率计数
+            for (int i = lo; i <= hi; i++)
+            {
+                count[CharAt(a[i], d)]++;
+            }
+
+            // 计算每个字符的种类的起始索引
+            for (int r = 0; r < R + 1; r++)
+            {
+                count[r + 1] += count[r];
+            }
+
+            for (int i = 0; i <= hi; i++)
+            {
+                _aux[count[CharAt(a[i], d) + 1]++] = a[i];
+            }
+
+            for (int i = lo; i <= hi; i++)
+            {
+                a[i] = _aux[i - lo];
+            }
+
+            for (int r = 0; r < R; r++)
+            {
+                Sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+            }
+        }
+    }
+}
