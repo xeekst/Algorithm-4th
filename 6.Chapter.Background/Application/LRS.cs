3b69567e43a2e0be9d7cf22c1e4cfc000b50ec50
xeekst <xeekst@outlook.com>
2020-12-15 00:38:27 +0800

    最长公共子字符串

---
+++ 6.Chapter.Background/Application/LRS.cs
@@ -0,0 +1,21 @@
+namespace Application
+{
+    public class LRS
+    {
+        public static string GetLongestSubString(string text)
+        {
+            int N = text.Length;
+            SuffixArray suffix = new SuffixArray(text);
+            string lrs = "";
+            for (int i = 1; i < N; i++)
+            {
+                int len = suffix.Lcp(i);
+                if (len > lrs.Length)
+                {
+                    lrs = suffix.Select(i).Substring(0, len);
+                }
+            }
+            return lrs;
+        }
+    }
+}
