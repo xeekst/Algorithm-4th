479a753c4e5384e77624f65b7f23ae6f2ae00179
xeekst <xeekst@outlook.com>
2020-12-15 22:34:25 +0800

    基础提交

---
+++ 6.Chapter.Background/SuffixArray/MinPQ.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace SuffixArray
+{
+    public class MinPQ<T> where T : IComparable<T>
+    {
+        private T[] _queue;
+        private int _count;
+
+        public int Count => _count;
+        public MinPQ(int N)
+        {
+            _queue = new T[N + 1];
+        }
+
+        public void Insert(T node)
+        {
+            int index = ++_count;
+            _queue[index] = node;
+            Swim(index);
+        }
+
+        public T DelMin()
+        {
+            T minNode = _queue[1];
+            _queue[1] = _queue[_count];
+            _queue[_count--] = default(T);
+            Sink(1);
+            return minNode;
+        }
+
+        private void Swim(int k)
+        {
+            while (k > 1)
+            {
+                if (_queue[k].CompareTo(_queue[k / 2]) < 0)
+                {
+                    Swap(_queue, k, k / 2);
+                    k = k / 2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (2 * k <= _count)
+            {
+                int minChild = 2 * k;
+                //找到两个子节点 更小的那个节点
+                if (minChild + 1 <= _count && _queue[minChild].CompareTo(_queue[minChild + 1]) > 0) minChild++;
+
+                if (_queue[k].CompareTo(_queue[minChild]) > 0)
+                {
+                    Swap(_queue, k, minChild);
+                    k = minChild;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Swap(T[] a, int i, int j)
+        {
+            T tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    }
+}
---
+++ 6.Chapter.Background/SuffixArray/SortAbstract.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace SuffixArray
+{
+    public abstract class SortAbstract
+    {
+        public abstract void Sort(IComparable[] a);
+
+        protected virtual bool Less(IComparable a, IComparable b)
+        {
+            if (a.CompareTo(b) < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        protected virtual void Swap(IComparable[] a, int i, int j)
+        {
+            IComparable tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+
+        public virtual void Show(IComparable[] a)
+        {
+            for (int index = 0; index < a.Length; index++)
+            {
+                Console.Write($"{a[index]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public virtual bool IsSorted(IComparable[] a)
+        {
+            for (int index = 0; index < a.Length - 1; index++)
+            {
+                if (a[index].CompareTo(a[index + 1]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
